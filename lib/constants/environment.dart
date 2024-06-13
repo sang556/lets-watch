b890e132d11120078d032752aae1aa8566d34c56
blackworm <lske888@163.com>
2024-06-13 17:12:38 +0800

    1.解决gradle环境问题

--- lib/constants/environment.dart
+++ lib/constants/environment.dart
@@ -1,5 +1,5 @@
 class Environment {
-  static const baseUrl = 'rtmp://192.168.1.100:1935';
-  static const baseRtspUrl = 'rtsp://192.168.1.100:8554';
-  static const baseHlsUrl = 'http://192.168.1.100:8888';
+  static const baseUrl = 'rtmp://120.79.163.124:1935/live';
+  static const baseRtspUrl = 'rtsp://120.79.163.124:554/live';
+  static const baseHlsUrl = 'http://192.168.1.100:8888/live';
 }
